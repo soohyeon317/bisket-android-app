171c09d5a6cabfeba77fdcf0fd1f09fdccd1215a
Soohyeon Park <soohyeon317@gmail.com>
2020-06-28 20:30:32 +0900

    [CHANGE] 화면구성 변경

--- app/src/main/java/com/bisket/MainActivity.kt
+++ app/src/main/java/com/bisket/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.bisket
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar!!.setTitle("비스켓")
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(0xFF8B0000.toInt()))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu1, menu)
+
+        return true
     }
 }
